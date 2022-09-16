d2ef98cfdb93e12edcfb36500df2e5509f1c121b
Arturs Artamonovs <arturs.artamonovs@protonmail.com>
2022-09-16 16:14:15 +0100

    Initial Commit

---
+++ IslandModel2/IslandModel2App.swift
@@ -0,0 +1,17 @@
+//
+//  IslandModel2App.swift
+//  IslandModel2
+//
+//  Created by Jacky Jack on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct IslandModel2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IslandModel2UITests/IslandModel2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IslandModel2UITestsLaunchTests.swift
+//  IslandModel2UITests
+//
+//  Created by Jacky Jack on 16/09/2022.
+//
+
+import XCTest
+
+class IslandModel2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
